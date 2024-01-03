5d081513452f67e7eede09f1844b71e275af286a
PrasannaMasina <147473093+PrasannaMasina@users.noreply.github.com>
2024-01-03 12:31:55 +0530

    #include<stdio.h> int main(){     int n,m;     scanf("%d %d",&n,&m);     int mat[n][m];     for(int i = 0 ; i < n ; i++){         for(int j = 0 ; j < m ; j++){             scanf("%d",&mat[i][j]);         }     }     int a = 0;     for(int i = 0 ; i < m ; i++){         int sum = 0;         for(int j = 0 ; j < n ; j++){             sum+=mat[j][a];         }         printf("%d ",sum);         a++;     } }

---
+++ Column_wise_sum_of_a_matrix.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    int mat[n][m];
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    int a = 0;
+    for(int i = 0 ; i < m ; i++){
+        int sum = 0;
+        for(int j = 0 ; j < n ; j++){
+            sum+=mat[j][a];
+        }
+        printf("%d ",sum);
+        a++;
+    }
+}

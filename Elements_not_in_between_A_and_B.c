47254b4feb505fe9e5d65ef5367386bf1ac1737e
PrasannaMasina <147473093+PrasannaMasina@users.noreply.github.com>
2024-03-03 11:59:10 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i = 0 ; i < n ; i++)     scanf("%d",&arr[i]);     int a,b,flag = 0;     scanf("%d %d",&a,&b);     for(int i = 0 ; i < n ; i++){         if(arr[i] < a || arr[i] > b){             printf("%d ",arr[i]);             flag = 1;         }     }     if(flag == 0) printf("-1"); }

---
+++ Elements_not_in_between_A_and_B.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i = 0 ; i < n ; i++)
+    scanf("%d",&arr[i]);
+    int a,b,flag = 0;
+    scanf("%d %d",&a,&b);
+    for(int i = 0 ; i < n ; i++){
+        if(arr[i] < a || arr[i] > b){
+            printf("%d ",arr[i]);
+            flag = 1;
+        }
+    }
+    if(flag == 0) printf("-1");
+}
